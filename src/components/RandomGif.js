cc39a28240fd38881f8c86946f149e7dae2d09cd
Harshul8824 <goswamiharshul88@gmail.com>
2025-03-02 23:36:56 +0530

    add some code

--- src/components/RandomGif.js
+++ src/components/RandomGif.js
@@ -1,13 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function RandomGif(){
-    let Gif = "";
-    function clickHandler(){
+    const API_KEY = process.env.REACT_APP_GIPHY_API_KEY
 
+    const Random = () =>{
+        const[rndGif,setrndGif] = useState("");
+        async function dataFetch(){
+            const url = `http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+             let data =await axios.get(url);
+             console.log(data);
+        }
+        useEffect( () =>{
+            dataFetch();
+        },[]);
+    }
+    Random();
+
+    function clickHandler(){
+      
     }
     return(
         <div className="randomgif random">
             <div>Random Gif</div>
-            <img src={Gif} alt="random image"></img>
+            {/* <img src={rndGif} alt="random image"></img> */}
             <button onClick={clickHandler}>Generate</button>
         </div>
     )
